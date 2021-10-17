68f3840cad021aa9cfd092d6cbdf9217e1d77ce9
Sergey Kvachonok <ravenexp@gmail.com>
2021-10-18 00:15:12 +0300

    Initial commit
    Create a new empty Rust library crate.
    
    Write a basic README.md description file.
    
    Add the standard MIT license file.

---
+++ README.md
@@ -0,0 +1,7 @@
+Planetarium
+===========
+
+Sub-pixel precision light spot rendering library for astronomy
+and video tracking applications.
+
+**Work In Progress**
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+//! Planetarium
+//! ===========
+//!
+//! Sub-pixel precision light spot rendering library for astronomy
+//! and video tracking applications.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
