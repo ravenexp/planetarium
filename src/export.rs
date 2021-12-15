9219f271bd1e2cae911c1b92f9eed3c2c263f228
Sergey Kvachonok <ravenexp@gmail.com>
2021-12-15 12:10:13 +0300

    feat: Add a public rectangle struct named `Window`
    Add a new public type representing a rectangular window on the canvas.
    It will be used as an argument for the windowed image export methods.
    
    Add struct definition docstrings and doctests.
    Add a constructor and a few utility methods.
    Define some common traits for the new type.
    
    TODO: Add windowed image export methods to `Canvas`.

--- src/export.rs
+++ src/export.rs
@@ -16,6 +16,53 @@ mod png;
 
 use crate::Canvas;
 
+/// Canvas image window coordinates
+///
+/// Defines a rectangular window on the canvas to export the image from.
+///
+/// The window origin is defined by its the upper left corner.
+///
+/// Basic operations
+/// ----------------
+///
+/// ```
+/// use planetarium::Window;
+///
+/// // Create a new rectangular window with origin at (0, 0).
+/// let wnd1 = Window::new(128, 64);
+///
+/// // Move the window origin to (250, 150).
+/// let wnd2 = wnd1.at(250, 150);
+///
+/// // Check the resulting string representation.
+/// assert_eq!(wnd2.to_string(), "(250, 150)+(128, 64)");
+/// ```
+///
+/// Conversions
+/// -----------
+///
+/// ```
+/// # use planetarium::Window;
+/// // From a tuple of tuples representing the origin coordinates
+/// // and window dimensions
+/// let wnd1 = Window::from(((100, 200), (128, 128)));
+///
+/// // Check the resulting string representation.
+/// assert_eq!(wnd1.to_string(), "(100, 200)+(128, 128)");
+/// ```
+
+#[derive(Debug, Clone, Copy)]
+pub struct Window {
+    /// Window origin X coordinate
+    pub x: u32,
+    /// Window origin Y coordinate
+    pub y: u32,
+    /// Width in X direction
+    pub w: u32,
+    /// Height in Y direction
+    pub h: u32,
+}
+
 /// Exportable canvas image formats
 #[derive(Debug, Clone, Copy)]
 #[non_exhaustive]
@@ -43,6 +90,21 @@ pub enum EncoderError {
     NotImplemented,
 }
 
+impl From<((u32, u32), (u32, u32))> for Window {
+    /// Creates a window from a tuple `((x, y), (w, h))`.
+    fn from(tuple: ((u32, u32), (u32, u32))) -> Self {
+        let ((x, y), (w, h)) = tuple;
+
+        Window { x, y, w, h }
+    }
+}
+
+impl std::fmt::Display for Window {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "({}, {})+({}, {})", self.x, self.y, self.w, self.h)
+    }
+}
+
 impl std::fmt::Display for EncoderError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         // FIXME: Put full length error descriptions here.
@@ -51,6 +113,38 @@ impl std::fmt::Display for EncoderError {
 }
 
 impl std::error::Error for EncoderError {}
+
+impl Window {
+    /// Creates a new window with given dimensions located at the origin.
+    pub fn new(width: u32, height: u32) -> Self {
+        Window {
+            x: 0,
+            y: 0,
+            w: width,
+            h: height,
+        }
+    }
+
+    /// Moves the window origin to the given origin coordinates.
+    pub fn at(&self, x: u32, y: u32) -> Window {
+        let w = self.w;
+        let h = self.h;
+
+        Window { x, y, w, h }
+    }
+
+    /// Checks if the window rectangle is inside the canvas rectangle.
+    #[allow(dead_code)]
+    fn is_inside(&self, width: u32, height: u32) -> bool {
+        self.x + self.w <= width && self.y + self.h <= height
+    }
+
+    /// Returns the total number of pixels in the window.
+    #[allow(dead_code)]
+    fn len(&self) -> usize {
+        (self.w * self.h) as usize
+    }
+}
 
 impl Canvas {
     /// Exports the canvas contents in the requested image format.
@@ -77,10 +171,11 @@ impl Canvas {
     }
 }
 
-#[cfg(all(test, not(feature = "png")))]
+#[cfg(test)]
 mod tests {
     use super::*;
 
+    #[cfg(not(feature = "png"))]
     #[test]
     fn image_format_error() {
         let c = Canvas::new(0, 0);
@@ -89,5 +184,15 @@ mod tests {
             c.export_image(ImageFormat::PngGamma8Bpp),
             Err(EncoderError::NotImplemented)
         );
+    }
+
+    #[test]
+    fn window_ops() {
+        let wnd = Window::new(128, 64).at(200, 100);
+
+        assert_eq!(wnd.len(), 128 * 64);
+        assert!(wnd.is_inside(400, 500));
+        assert!(!wnd.is_inside(100, 100));
+        assert!(!wnd.at(300, 100).is_inside(400, 500));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -150,7 +150,7 @@ mod export;
 mod gamma;
 mod pattern;
 
-pub use crate::export::{EncoderError, ImageFormat};
+pub use crate::export::{EncoderError, ImageFormat, Window};
 
 use crate::gamma::GammaCurve8;
 use crate::pattern::AiryPattern;
